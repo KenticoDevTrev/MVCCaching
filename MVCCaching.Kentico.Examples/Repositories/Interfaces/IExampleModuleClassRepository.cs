dc4f4ff4ce3feb36507a1668c5c8b5bda4165253
Trevor Fayas <tfayas@hbs.net>
2019-09-19 17:52:16 -0500

    Adding more examples

---
+++ MVCCaching.Kentico.Examples/Repositories/Interfaces/IExampleModuleClassRepository.cs
@@ -0,0 +1,32 @@
+﻿using MVCCaching.Kentico;
+using MVCCaching.Kentico.Example;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCCaching.Kentico.Example
+{
+    public interface IExampleModuleClassRepository : IRepository
+    {
+        #region "These are abstracted out to Generic Models"
+
+        IEnumerable<ExampleModuleClassModel> GetExampleModuleClasses();
+
+        ExampleModuleClassModel GetExampleModuleClass(int ID);
+
+        IEnumerable<string> GetExampleModuleClassesCacheDependency();
+
+        IEnumerable<string> GetExampleModuleClassCacheDependency(int ID);
+
+        #endregion
+
+        #region "These are returning BaseInfo/TreeNode Models"
+
+        ExampleModuleClassInfo GetExampleModuleClass_BaseInfo(int ID);
+
+        IEnumerable<ExampleModuleClassInfo> GetExampleModuleClasses_BaseInfo();
+
+        #endregion
+    }
+}
