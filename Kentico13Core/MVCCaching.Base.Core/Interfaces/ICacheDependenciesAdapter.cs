f40eb734370f026daa2daa71b280a793a8f6dbe0
Trevor Fayas <trevor.fayas@movement.com>
2023-10-25 22:31:28 -0500

    Add Cache Dependency MVC Tag Helper with kentico agnostic interface (with kentico implementation) so can use cache dependency in Kentico Agnostic way.

---
+++ Kentico13Core/MVCCaching.Base.Core/Interfaces/ICacheDependenciesAdapter.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Primitives;
+using System;
+
+namespace MVCCaching
+{
+    /// <summary>
+    /// Adapts dependency cache keys to change tokens.
+    /// </summary>
+    /// <seealso cref="IMemoryCache"/>
+    public interface ICacheDependenciesAdapter
+    {
+        /// <summary>
+        /// Gets a change token for an array of dependency cache keys.
+        /// </summary>
+        /// <param name="dependencyCacheKeys">Dependency cache keys to watch.</param>
+        /// <returns>Returns an <see cref="IChangeToken"/> for watching changes of the <paramref name="dependencyCacheKeys"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="dependencyCacheKeys"/> is null.</exception>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="dependencyCacheKeys"/> is an empty array.</exception>
+        IChangeToken GetChangeToken(string[] dependencyCacheKeys);
+    }
+}
---
+++ Kentico13Core/XperienceCommunity.MVCCaching/Implementations/CacheDependenciesAdapater.cs
@@ -0,0 +1,21 @@
+﻿using CMS.Helpers.Caching;
+using Microsoft.Extensions.Primitives;
+using MVCCaching;
+
+namespace XperienceCommunity.MVCCaching.Implementations
+{
+    public class CacheDependenciesAdapater : ICacheDependenciesAdapter
+    {
+        private readonly ICacheDependencyAdapter _cacheDependencyAdapter;
+
+        public CacheDependenciesAdapater(ICacheDependencyAdapter cacheDependencyAdapter)
+        {
+            _cacheDependencyAdapter = cacheDependencyAdapter;
+        }
+
+        public IChangeToken GetChangeToken(string[] dependencyCacheKeys)
+        {
+            return _cacheDependencyAdapter.GetChangeToken(dependencyCacheKeys); 
+        }
+    }
+}
